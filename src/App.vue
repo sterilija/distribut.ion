c209a0eb0b5198697fdcb05fffe30412477b1fea
WzrywProstaty <joint353@gmail.com>
2023-10-28 15:44:16 +0300

    initialized the project structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+ import ListsItems from "./components/Lists-Items.vue";
+ export default {
+   components:{
+     ListsItems,
+   }
+ }
+</script>
+
+<template>
+  <div>
+      <ListsItems></ListsItems>
+    <div class="quads-container"></div>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
