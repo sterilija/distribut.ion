38c9124b282d4620cc1970d913269bc2852d6f83
WzrywProstaty <joint353@gmail.com>
2023-10-28 15:42:53 +0300

    Started creating a component does the LIST part of the task

---
+++ src/components/Lists-Items.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li class="d-flex_i gap-1_i">
+      <p>Item 1</p> <!--Item title-->
+      <p>10</p> <!--Item count-->
+      <input v-model="clrPick" type="color" name="" id=""> <!--Item color-->
+      <p>{{clrPick}}</p>
+    </li>
+    <li class="d-flex_i gap-1_i">
+      <p>Item 2</p> <!--Item title-->
+      <p>15</p> <!--Item count-->
+      <input v-model="clrPick" type="color" name="" id=""> <!--Item color-->
+      <p>{{clrPick}}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Lists-Items",
+  data:()=>({
+    clrPick: [],
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
